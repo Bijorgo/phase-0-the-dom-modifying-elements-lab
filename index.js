cf5791c6004197ff1c53714fcd2d40fb1daa9324
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-11-05 20:16:53 -0700

    submit dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//remove <main> w/ id 'main'
+//has a 'newHeader' variable that points to an <h1> node
+main.remove("main")
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = ("is the champion")
+document.body.appendChild(newHeader);
